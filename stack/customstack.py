90419b4353302ce65d148395a3fff081aae0242f
itshowrohitworks <therohitshrivastava@gmail.com>
2026-05-23 19:15:44 +0530

    Custom Stack Implementation.

---
+++ stack/customstack.py
@@ -0,0 +1,38 @@
+class Stack:
+    def __init__(self) -> None:
+        self.stack = []
+
+    def push(self,value):
+        self.stack.append(value)
+        
+    def is_empty(self):
+        return (len(self.stack) == 0)    
+
+    def pop(self):
+        if self.is_empty():
+            raise IndexError("Stack is Empty!")
+        return self.stack.pop()
+    
+    def peek(self):
+        if self.is_empty():
+            return None
+        return self.stack[-1]
+    
+    def size(self):
+        return len(self.stack)
+
+    def print_stack(self):
+        if self.is_empty():
+            print("Stack is Empty!")
+            return
+        for i in range(self.size() - 1, -1, -1):
+            display_item = str(self.stack[i])
+            print(f"|{display_item}|")
+
+
+mystack = Stack()
+mystack.push(5)
+mystack.push(6)
+mystack.push(7)
+mystack.push(8)
+mystack.print_stack()
